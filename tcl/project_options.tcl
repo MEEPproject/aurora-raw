cccdcd9ed6049a3007b2291398917ddd9031398c
Daniel J.Mazure <daniel.jimenez2@bsc.es>
2022-01-11 00:02:41 +0100

    Fix the IP description

--- tcl/project_options.tcl
+++ tcl/project_options.tcl
@@ -1,4 +1,4 @@
 set g_ip_version_min 0
 set g_ip_version_maj 1
 set g_ip_version ${g_ip_version_maj}.${g_ip_version_min}
-set g_ip_description "Aurora DMA-based IP"
+set g_ip_description "Aurora RAW IP"
