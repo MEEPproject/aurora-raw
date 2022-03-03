be850eda6d967b43bf6fe02f11636f7c97566b2e
Daniel J.Mazure <daniel.jimenez2@bsc.es>
2022-03-03 19:11:52 +0100

    Add license headers to tcl and vhd files

--- ip/aurora6466b.tcl
+++ ip/aurora6466b.tcl
@@ -1,3 +1,22 @@
+# Copyright 2022 Barcelona Supercomputing Center-Centro Nacional de Supercomputación
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Francelly Cano, BSC-CNS
+# Date: 22.02.2022
+# Description: 
+
+
 create_ip -name aurora_64b66b -vendor xilinx.com -library ip -version 12.0 -module_name aurora_64b66b_0
 
 set_property -dict [list CONFIG.drp_mode {Native} CONFIG.SupportLevel {1}] [get_ips aurora_64b66b_0]
